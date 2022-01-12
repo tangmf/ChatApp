79bbe60e0d3824de6bd7e7cfb68941db47183503
Natalie Long <natalie.long1818@gmail.com>
2022-01-12 10:26:10 +0800

    Add speech to text js

--- public/script.js
+++ public/script.js
@@ -104,10 +104,32 @@ recognition.onstart = function(){
   instructions.text("Voice recognition is on")
 }
 
+recognition.onspeechend = function(){
+  instructions.text("No Activity")
+}
+
+recognition.onerror = function(){
+  instructions.text("Try Again")
+}
+
+recognition.onresult = function(event){
+  var current = event.resultIndex;
+
+  var transcript = event.results[current][0].transcript
+
+  content += transcript
+
+  textbox.val(content)
+}
+
 $("#start-btn").click(function(event){
   if(content.length){
     content += ''
   }
 
   recognition.start()
+})
+
+textbox.on('Input', function(){
+  content = $(this).val()
 })
